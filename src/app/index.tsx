3bf126a637ea017603d1170a6d9840758f0a2376
Yuriy85 <yurik8585@gmail.com>
2024-02-26 15:43:31 +0300

    feat: config environment

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <h1>Valantis Test</h1>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+interface Data {}
+
+const data: Data = {};
+
+export default data;
